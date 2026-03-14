8f05a614a4205b416c6c2fb499420e7a1d22f614
paper209 <fleshed-stroll84@icloud.com>
2026-03-14 14:04:47 +0900

    Add Tor proxy checking in checker.go
    Implement Tor proxy checking functionality to filter proxies.

--- server/proxy/checker.go
+++ server/proxy/checker.go
@@ -1,5 +1,66 @@
 package proxy
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	px "golang.org/x/net/proxy"
+)
+
+type Response struct {
+	IsTor bool `json:"IsTor"`
+}
+
+const checkAPI string = "https://check.torproject.org/api/ip"
+
+func isTor(proxy string) (bool, error) {
+	dialer, err := px.SOCKS5("tcp", proxy, nil, px.Direct)
+	if err != nil {
+		return false, fmt.Errorf("Proxy Error: %s", err.Error())
+	}
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			DialContext: func(ctx context.Context, network, proxy string) (net.Conn, error) {
+				return dialer.Dial(network, proxy)
+			},
+		},
+	}
+
+	resp, err := client.Get(checkAPI)
+	if err != nil {
+		return false, fmt.Errorf("Check Proxy Error: %s", err.Error())
+	}
+	defer resp.Body.Close()
+
+	var response Response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return false, fmt.Errorf("Check Proxy Error: %s", err.Error())
+	}
+
+	return response.IsTor, nil
+}
+
 func checkProxies(proxies []string) []string {
-	return proxies // 임시
+	var result []string
+	for _, p := range proxies {
+		stat, err := isTor(p)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		if stat {
+			result = append(result, p)
+		}
+	}
+
+	return result
 }
