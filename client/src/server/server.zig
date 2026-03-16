5078d1b6601adaf6f3f8ac5036268e6978fea4a9
paper209 <fleshed-stroll84@icloud.com>
2026-03-17 04:02:10 +0900

    Use config for server address and port

--- client/src/server/server.zig
+++ client/src/server/server.zig
@@ -1,12 +1,11 @@
 const std = @import("std");
+
+const config = @import("../config.zig");
 pub const client = @import("client.zig");
 pub const proxy = @import("../proxy/proxy.zig");
 
-const server_address = ".onion";
-const server_port: u16 = 2222;
-
 pub fn connect(group: []const u8, p: []const u8) !void {
-    var stream = try proxy.socks5.connect(p, server_address, server_port);
+    var stream = try proxy.socks5.connect(p, config.server_address, config.server_port);
     defer stream.close();
 
     try client.handler(stream, group);
