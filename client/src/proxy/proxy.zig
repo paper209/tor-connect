e60e1c008f763e0348ce7cc29e556972832ae8dd
paper209 <fleshed-stroll84@icloud.com>
2026-03-17 14:40:48 +0900

    Add print_proxies function and update proxy handling
    Added a function to print the list of proxies and updated the update_proxies function to duplicate the proxy strings before appending.

--- client/src/proxy/proxy.zig
+++ client/src/proxy/proxy.zig
@@ -1,25 +1,35 @@
 const std = @import("std");
-pub const socks5 = @import("socks5.zig");
-pub var tor_proxies: std.ArrayList([]const u8) = undefined;
 const allocator = std.heap.page_allocator;
+
+pub const socks5 = @import("socks5.zig");
+
+pub var tor_proxies: std.ArrayList([]const u8) = undefined;
+
+fn print_proxies() void {
+    for (tor_proxies.items) |proxy| {
+        std.debug.print("{s}\n", .{proxy});
+    }
+}
 
 pub fn init(proxies: []const []const u8) !void {
     tor_proxies = try .initCapacity(allocator, 0);
     for (proxies) |proxy| {
         try tor_proxies.append(allocator, proxy);
     }
+
+    //print_proxies();
 }
 
 pub fn update_proxies(data: []const u8) !void {
+    print_proxies();
     tor_proxies.deinit(allocator);
     tor_proxies = try .initCapacity(allocator, 0);
 
     var proxies = std.mem.splitScalar(u8, data, ' ');
     while (proxies.next()) |proxy| {
-        try tor_proxies.append(allocator, proxy);
+        const proxy_copy = try allocator.dupe(u8, proxy);
+        try tor_proxies.append(allocator, proxy_copy);
     }
 
-    for (tor_proxies.items) |proxy| {
-        std.debug.print("{s}\n", .{proxy});
-    }
+    //print_proxies();
 }
